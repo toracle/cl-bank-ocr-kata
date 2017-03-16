4b2e80c86d9b4c66db77b410a6a67fad479e8d1c
Jeongsoo Park <toracle@gmail.com>
2017-03-15 18:30:11 -0700

    apply DSL style

--- src/base.lisp
+++ src/base.lisp
@@ -1,15 +1,20 @@
+(defvar *ocr-pattern-to-val* '(("   ~%  |~%  |~%   ~%" . 1)
+                               (" _ ~% _|~%|_ ~%   ~%" . 2)
+                               (" _ ~% _|~% _|~%   ~%" . 3)
+                               ("   ~%|_|~%  |~%   ~%" . 4)
+                               (" _ ~%|_ ~% _|~%   ~%" . 5)
+                               (" _ ~%|_ ~%|_|~%   ~%" . 6)
+                               (" _ ~%  |~%  |~%   ~%" . 7)
+                               (" _ ~%|_|~%|_|~%   ~%" . 8)
+                               (" _ ~%|_|~% _|~%   ~%" . 9)))
+
 (defun string-format-match (str fmt)
   (string= str (format nil fmt)))
 
-
 (defun parse-digit (str)
   "parse digit of STR"
-  (cond ((string-format-match str "   ~%  |~%  |~%   ~%") 1)
-	((string-format-match str " _ ~% _|~%|_ ~%   ~%") 2)
-	((string-format-match str " _ ~% _|~% _|~%   ~%") 3)
-	((string-format-match str "   ~%|_|~%  |~%   ~%") 4)
-	((string-format-match str " _ ~%|_ ~% _|~%   ~%") 5)
-	((string-format-match str " _ ~%|_ ~%|_|~%   ~%") 6)
-	((string-format-match str " _ ~%  |~%  |~%   ~%") 7)
-	((string-format-match str " _ ~%|_|~%|_|~%   ~%") 8)
-	((string-format-match str " _ ~%|_|~% _|~%   ~%") 9)))
+  (loop for pattern-to-val in *ocr-pattern-to-val*
+     for pattern = (car pattern-to-val)
+     for val = (cdr pattern-to-val)
+     when (string-format-match str pattern)
+     return val))
